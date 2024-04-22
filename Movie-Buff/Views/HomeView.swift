89229e1d91ac943edab0633ab44e8ee3041f511c
mohak92 <tamhane.mohak@gmail.com>
2024-04-22 13:13:50 -0400

    rename file and update references

--- Movie-Buff/Views/HomeView.swift
+++ Movie-Buff/Views/HomeView.swift
@@ -8,54 +8,24 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+struct HomeView: View {
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        TabView {
+            MovieListView()
+                .tabItem { Label("Movie", systemImage: "film") }
+            TVShowsListView()
+                .tabItem { Label("TV", systemImage: "tv.fill") }
+            ActorListView()
+                .tabItem { Label("Actor", systemImage: "rectangle.portrait.on.rectangle.portrait.angled.fill") }
+            FavoriteMovieListView()
+                .tabItem { Label("Lists", systemImage: "heart.circle") }
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
         .modelContainer(for: Item.self, inMemory: true)
 }
