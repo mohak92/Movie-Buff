7097c9253672b49c4054fe4ae0f77cce00d2f827
mohak92 <tamhane.mohak@gmail.com>
2024-04-25 09:04:37 -0400

    api endpoints

--- Movie-Buff/App/Constants.swift
+++ Movie-Buff/App/Constants.swift
@@ -2,18 +2,31 @@
 //  URLUtils.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/22/24.
+//  Created by Mohak Tamhane on 4/18/24.
 //
 
 import Foundation
 
-enum Urls {
-    static let baseURL = "https://api.themoviedb.org/3"
-    static let upcomingED = baseURL + "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=2024-02-17&primary_release_date.lte=2024-12-31&sort_by=popularity.desc&with_release_type=2&year=2024"
-    static let genresED = baseURL + "/genre/movie/list?language=en"
-    static let trendingED = baseURL + "/trending/movie/day?language=en-US"
-    static let topRatedED = baseURL + "/movie/top_rated?language=en-US&page=1"
-    static let nowPlayingED = baseURL + "/movie/now_playing?language=en-US&page=1"
-    static let trendThai = baseURL + "/discover/movie?include_adult=false&include_video=false&language=hi-IN&page=1&primary_release_year=2024&sort_by=popularity.desc&with_original_language=hi"
-    static let popularPerson = baseURL + "/person/popular?language=en-US&page=1"
+enum Constants {
+    enum URLs {
+        public static let baseURL = "https://api.themoviedb.org/3"
+
+        // swiftlint:disable:next line_length
+        public static let upcoming = baseURL + "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1"
+        public static let releaseDateGTE = "&primary_release_date.gte="
+        public static let releaseDateLTE = "&primary_release_date.lte="
+        public static let sortUpcoming = "&sort_by=popularity.desc&with_release_type=2&year="
+
+        public static let genres = baseURL + "/genre/movie/list?language=en"
+        public static let trending = baseURL + "/trending/movie/day?language=en-US"
+        public static let topRated = baseURL + "/movie/top_rated?language=en-US&page=1"
+        public static let nowPlaying = baseURL + "/movie/now_playing?language=en-US&page=1"
+
+        // swiftlint:disable:next line_length
+        public static let trendBollywood = baseURL + "/discover/movie?include_adult=false&include_video=false&language=hi-IN"
+        public static let primaryReleaseYearBollywood = "&page=1&primary_release_year="
+        public static let sortBollywood = "&sort_by=popularity.desc&with_original_language=hi"
+
+        public static let popularPerson = baseURL + "/person/popular?language=en-US&page=1"
+    }
 }
