f6435a660234698a1f6d171ee12635d2ff5dcda1
mohak92 <tamhane.mohak@gmail.com>
2024-04-25 09:04:00 -0400

    create movie model

--- Movie-Buff/Models/Movie.swift
+++ Movie-Buff/Models/Movie.swift
@@ -2,7 +2,50 @@
 //  Movie.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/22/24.
+//  Created by Mohak Tamhane on 4/18/24.
 //
 
 import Foundation
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable, Identifiable, Hashable {
+
+    var id: Int
+    var title: String
+    var overview: String?
+    var releaseDate: String
+    var originalLanguage: String
+    var genreIds: [Int]
+    var posterPath: String?
+    var posterURL: URL? {
+        if let poster = posterPath {
+            return URL(string: "https://image.tmdb.org/t/p/w500" + poster)
+        }
+        return nil
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case releaseDate = "release_date"
+        case originalLanguage = "original_language"
+        case genreIds = "genre_ids"
+        case posterPath = "poster_path"
+    }
+}
+
+struct Mockdata {
+    // swiftlint:disable:next line_length
+    static let sampledata = Movie(id: 0, title: "", overview: "", releaseDate: "", originalLanguage: "", genreIds: [0], posterPath: "")
+
+    static let movielist = [
+        Movie(id: 0, title: "", overview: "", releaseDate: "", originalLanguage: "", genreIds: [0], posterPath: ""),
+        Movie(id: 1, title: "", overview: "", releaseDate: "", originalLanguage: "", genreIds: [0], posterPath: ""),
+        Movie(id: 2, title: "", overview: "", releaseDate: "", originalLanguage: "", genreIds: [0], posterPath: ""),
+        Movie(id: 3, title: "", overview: "", releaseDate: "", originalLanguage: "", genreIds: [0], posterPath: "")
+    ]
+}
