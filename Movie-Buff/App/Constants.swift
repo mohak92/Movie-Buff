5f9e88e952b4b6a56aeacd28dddc79010077fbca
mohak92 <tamhane.mohak@gmail.com>
2024-04-25 09:19:58 -0400

    alert messages

--- Movie-Buff/App/Constants.swift
+++ Movie-Buff/App/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum Constants {
     enum URLs {
@@ -29,4 +30,53 @@ enum Constants {
 
         public static let popularPerson = baseURL + "/person/popular?language=en-US&page=1"
     }
+}
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertMessage {
+    static let invalidData = AlertItem(title: Text("Server Error"),
+                                       // swiftlint:disable:next line_length
+                                   message: Text("The data receive from the server was invalid, Please contact support."),
+                                   dismissButton: .default(Text("OK")))
+
+    static let invalidResponse = AlertItem(title: Text("Server Error"),
+                                           // swiftlint:disable:next line_length
+                                       message: Text("Invalid response from the server. Please try again later or contact support."),
+                                       dismissButton: .default(Text("OK")))
+
+    static let invalidURL = AlertItem(title: Text("Server Error"),
+                                      // swiftlint:disable:next line_length
+                                  message: Text("There was connecting issue to the server. If this persists, Please contact support."),
+                                  dismissButton: .default(Text("OK")))
+
+    static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                            // swiftlint:disable:next line_length
+                                        message: Text("Unable to complete your request this time. Please check you network."),
+                                        dismissButton: .default(Text("OK")))
+
+    static let invalidForm = AlertItem(title: Text("Form not complete"),
+                                   message: Text("Please fill all the info."),
+                                   dismissButton: .default(Text("OK")))
+
+    static let invalidEmail = AlertItem(title: Text("Form not complete"),
+                                    message: Text("Email invalid."),
+                                    dismissButton: .default(Text("OK")))
+
+    static let userSaveSuccess = AlertItem(title: Text("Profile"),
+                                       message: Text("Your Information was successfully done."),
+                                       dismissButton: .default(Text("OK")))
+
+    static let invalidUserData = AlertItem(title: Text("Profile"),
+                                       message: Text("Your Information save/retrieve process can't be done."),
+                                       dismissButton: .default(Text("OK")))
+
+    static let GeneralError = AlertItem(title: Text("Willy Nilly"),
+                                    message: Text("Something wrong with the data.\nPlease contact support."),
+                                    dismissButton: .default(Text("OK")))
 }
