6973ff06f6492c7fed3c30e499de4cb96eeeb343
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:21:16 -0400

    trending actor model

--- Movie-Buff/Models/TrendingActor.swift
+++ Movie-Buff/Models/TrendingActor.swift
@@ -2,7 +2,40 @@
 //  TrendingActor.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/25/24.
+//  Created by Mohak Tamhane on 4/23/24.
 //
 
 import Foundation
+
+struct TrendingActorResponse: Codable {
+    let results: [TrendingActor]
+}
+
+struct ActorMovieCreditsResponse: Codable {
+    let cast: [Movie]
+}
+
+struct TrendingActor: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let originalName: String
+    let knownForDepartment: String?
+    let popularity: Double
+
+    let profilePath: String?
+    var profileURL: URL? {
+        if let poster = profilePath {
+            return URL(string: "https://image.tmdb.org/t/p/w500" + poster)
+        }
+        return nil
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case originalName = "original_name"
+        case knownForDepartment = "known_for_department"
+        case popularity
+        case profilePath = "profile_path"
+    }
+}
