0e24fa734210f14b100b49ad71f2f4a222364b78
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:23:41 -0400

    viewModel add network calls to fetch data

--- Movie-Buff/ViewModel/MovieService.swift
+++ Movie-Buff/ViewModel/MovieService.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 
+// swiftlint:disable:next type_body_length
 class MovieService {
 
     private let session: URLSession
@@ -161,6 +162,217 @@ class MovieService {
 
             let decoder = JSONDecoder()
             return try decoder.decode(MovieResponse.self, from: data).results
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadMovieDetail(movieID: String) async throws -> MovieDetail {
+        // swiftlint:disable:next line_length
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)?append_to_response=results&language=en-US") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(MovieDetail.self, from: data)
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadGenres() async throws -> [Genre] {
+        guard let url = URL(string: Constants.URLs.genres) else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(GenreListResponse.self, from: data).genres
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadMovieByGenre(genreID: String, page: Int) async throws -> [Movie] {
+        // swiftlint:disable:next line_length
+        guard let url = URL(string: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=\(page)&sort_by=popularity.desc&with_genres=\(genreID)") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(MovieResponse.self, from: data).results
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadMovieSimilar(movieID: String) async throws -> [Movie] {
+        // swiftlint:disable:next line_length
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/similar?language=en-US&page=1") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(MovieResponse.self, from: data).results
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadCredits(movieID: String) async throws -> [Cast] {
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/credits?language=en-US") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(CreditsResponse.self, from: data).cast
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadCastDetail(castID: String) async throws -> ActorDetail {
+
+        guard let url = URL(string: "https://api.themoviedb.org/3/person/\(castID)?language=en-US") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            //            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            //            print(json)
+            let decoder = JSONDecoder()
+            return try decoder.decode(ActorDetail.self, from: data)
+        } catch {
+            print("Error converting JSON data to object: \(error)")
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadCastImages(castID: Int) async throws -> [ActorImage] {
+        guard let url = URL(string: "https://api.themoviedb.org/3/person/\(castID)/images") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(ActorImageResponse.self, from: data).profiles
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadMovieReview(movieID: String) async throws -> [MovieReview] {
+        // swiftlint:disable:next line_length
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/reviews?language=en-US&page=1") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(MovieReviewResponse.self, from: data).results
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadPopularPerson() async throws -> [TrendingActor] {
+        guard let url = URL(string: Constants.URLs.popularPerson) else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            //            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            //            print(json)
+            let decoder = JSONDecoder()
+            return try decoder.decode(TrendingActorResponse.self, from: data).results
+        } catch {
+            //            print("Error converting JSON data to object: \(error)")
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadPersonMovieCredits(personId: Int) async throws -> [Movie] {
+        // swiftlint:disable:next line_length
+        guard let url = URL(string: "https://api.themoviedb.org/3/person/\(personId)/movie_credits?language=en-US") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(ActorMovieCreditsResponse.self, from: data).cast
+        } catch {
+            throw LoadMovieDataError.invalidData
+        }
+    }
+
+    func loadMovieImage(id: Int) async throws -> [MovieImage] {
+
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/images") else {
+            throw LoadMovieDataError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+
+            let decoder = JSONDecoder()
+            return try decoder.decode(MovieImageResponse.self, from: data).backdrops
         } catch {
             throw LoadMovieDataError.invalidData
         }
--- Movie-Buff/ViewModel/MovieViewModel.swift
+++ Movie-Buff/ViewModel/MovieViewModel.swift
@@ -16,9 +16,26 @@ final class MovieViewModel: ObservableObject, Observable {
     @Published var searchMovie: [Movie] = []
     @Published var trendingBollywood: [Movie] = []
 
+    @Published var genre: [Genre] = []
+    @Published var movieByGenre: [Movie] = []
+
+    @Published var movieDetail: MovieDetail = Mockdata.movieDetailsample
+    @Published var movieSimilar: [Movie] = []
+    @Published var movieReview: [MovieReview] = []
+    @Published var movieImage: [MovieImage] = []
+    @Published var movieCredits: [Cast] = []
+
+    @Published var cast: [Cast] = []
+    @Published var actorDetail: ActorDetail = Mockdata.actorDetailSample
+    @Published var actorImage: [ActorImage] = []
+    @Published var trendingActor: [TrendingActor] = []
+    @Published var actorMovieCredits: [Movie] = []
+
     @Published var isLoading: Bool = false
 
     @Published var alertItem: AlertItem?
+
+    @Published var likedMovies: [MovieItem] = []
 
     func getUpcomingMovie() {
         isLoading = true
@@ -91,6 +108,150 @@ final class MovieViewModel: ObservableObject, Observable {
             do {
                 // swiftlint:disable:next line_length
                 searchMovie.append(contentsOf: try await MovieService.shared.loadMovieSearchResults(query: query, page: page))
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getMovieDetail(movieID: String) {
+        isLoading = true
+        Task {
+            do {
+                movieDetail = try await MovieService.shared.loadMovieDetail(movieID: movieID)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getGenre() {
+        isLoading = true
+        Task {
+            do {
+                genre = try await MovieService.shared.loadGenres()
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getMovieByGenre(genreID: String, page: Int) {
+        isLoading = true
+        Task {
+            do {
+                // swiftlint:disable:next line_length
+                movieByGenre.append(contentsOf: try await MovieService.shared.loadMovieByGenre(genreID: genreID, page: page))
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getMovieSimilar(movieID: String) {
+        isLoading = true
+        Task {
+            do {
+                movieSimilar = try await MovieService.shared.loadMovieSimilar(movieID: movieID)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getMovieCredits(movieID: String) {
+        isLoading = true
+        Task {
+            do {
+                movieCredits = try await MovieService.shared.loadCredits(movieID: movieID)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getCastDetail(cast: String) {
+        isLoading = true
+        Task {
+            do {
+                actorDetail = try await MovieService.shared.loadCastDetail(castID: cast)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getCastImages(cast: Int) {
+        isLoading = true
+        Task {
+            do {
+                actorImage = try await MovieService.shared.loadCastImages(castID: cast)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getMovieReview(movieID: String) {
+        isLoading = true
+        Task {
+            do {
+                movieReview = try await MovieService.shared.loadMovieReview(movieID: movieID)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getTrendingActor() {
+        isLoading = true
+        Task {
+            do {
+                trendingActor = try await MovieService.shared.loadPopularPerson()
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getActorMovieCredits(personId: Int) {
+        isLoading = true
+        Task {
+            do {
+                actorMovieCredits = try await MovieService.shared.loadPersonMovieCredits(personId: personId)
+                isLoading = false
+            } catch {
+                alertItem = AlertMessage.GeneralError
+                isLoading = false
+            }
+        }
+    }
+
+    func getMovieImage(id: Int) {
+        isLoading = true
+        Task {
+            do {
+                movieImage = try await MovieService.shared.loadMovieImage(id: id)
                 isLoading = false
             } catch {
                 alertItem = AlertMessage.GeneralError
