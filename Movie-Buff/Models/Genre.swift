04927a5f8ebbb3be32be8d943a2845675eea0188
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:18:43 -0400

    generes model

--- Movie-Buff/Models/Genre.swift
+++ Movie-Buff/Models/Genre.swift
@@ -2,7 +2,16 @@
 //  Genre.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/25/24.
+//  Created by Mohak Tamhane on 4/23/24.
 //
 
 import Foundation
+
+struct GenreListResponse: Codable {
+    let genres: [Genre]
+}
+
+struct Genre: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
