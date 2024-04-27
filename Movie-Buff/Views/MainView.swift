7a8240c224d4576ab5deb313c4264eea7c7e4a2b
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:28:34 -0400

    load onboarding view and app view

--- Movie-Buff/Views/MainView.swift
+++ Movie-Buff/Views/MainView.swift
@@ -2,14 +2,26 @@
 //  MainView.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/26/24.
+//  Created by Mohak Tamhane on 4/24/24.
 //
 
 import SwiftUI
 
 struct MainView: View {
+
+    @StateObject var movieViewModel = MovieViewModel()
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+                    .environmentObject(movieViewModel)
+                    .modelContainer(for: [MovieItem.self])
+            }
+        }
     }
 }
 
