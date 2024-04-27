9bab6f43f7e0a4a572a649c7ee14b09b83821b71
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:20:48 -0400

    movie review model

--- Movie-Buff/Models/MovieReview.swift
+++ Movie-Buff/Models/MovieReview.swift
@@ -2,7 +2,35 @@
 //  MovieReview.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/25/24.
+//  Created by Mohak Tamhane on 4/23/24.
 //
 
 import Foundation
+
+struct MovieReviewResponse: Codable {
+    let results: [MovieReview]
+}
+
+struct MovieReview: Codable {
+    let author: String
+    let authorDetails: AuthorDetails
+    let content: String
+
+    enum CodingKeys: String, CodingKey {
+        case author
+        case authorDetails = "author_details"
+        case content
+    }
+}
+
+struct AuthorDetails: Codable, Hashable {
+    let name: String
+    let username: String
+    let avatarPath: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case username
+        case avatarPath = "avatar_path"
+    }
+}
