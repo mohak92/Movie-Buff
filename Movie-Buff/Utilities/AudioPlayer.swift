ba5d4fd91d85d99e2bfd7d795d68ce7bc029de6b
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:22:19 -0400

    AVFoundation audio player

--- Movie-Buff/Utilities/AudioPlayer.swift
+++ Movie-Buff/Utilities/AudioPlayer.swift
@@ -2,7 +2,21 @@
 //  AudioPlayer.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/26/24.
+//  Created by Mohak Tamhane on 4/24/24.
 //
 
 import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playAudio(filename: String, fileType: String) {
+    if let path = Bundle.main.path(forResource: filename, ofType: fileType) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Cannot play audio")
+        }
+    }
+}
