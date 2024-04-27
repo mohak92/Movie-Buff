c331de3b18a747fe3e2db064a51cc51c060c292b
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:20:16 -0400

    movie image model

--- Movie-Buff/Models/MovieImage.swift
+++ Movie-Buff/Models/MovieImage.swift
@@ -2,7 +2,28 @@
 //  MovieImage.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/25/24.
+//  Created by Mohak Tamhane on 4/23/24.
 //
 
 import Foundation
+
+struct MovieImageResponse: Codable {
+    let backdrops: [MovieImage]
+}
+
+struct MovieImage: Codable {
+    let filePath: String?
+    let voteAverage: Float
+
+    var imageURL: URL? {
+        if let filepath = filePath {
+            return URL(string: "https://image.tmdb.org/t/p/w500" + filepath)
+        }
+        return nil
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case filePath = "file_path"
+        case voteAverage = "vote_average"
+    }
+}
