cca36c4a425097b99cc5683949d680ef55ed30f7
mohak92 <tamhane.mohak@gmail.com>
2024-04-26 20:32:33 -0400

    movie info on movie detail view

--- Movie-Buff/Views/MovieInfoView.swift
+++ Movie-Buff/Views/MovieInfoView.swift
@@ -2,17 +2,69 @@
 //  MovieInfoView.swift
 //  Movie-Buff
 //
-//  Created by Mohak Tamhane on 4/25/24.
+//  Created by Mohak Tamhane on 4/23/24.
 //
 
 import SwiftUI
 
 struct MovieInfoView: View {
+
+    var movieDetail: MovieDetail
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Capsule()
+                    .strokeBorder(Color("BorderColor"), lineWidth: 1)
+                    .frame(width: 60, height: 25)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .opacity(0.6)
+                    .overlay {
+                        HStack {
+                            Image(systemName: "star.fill")
+                                .resizable()
+                                .foregroundStyle(.yellow)
+                                .frame(width: 12, height: 12)
+                            Text("\(movieDetail.voteAverage, specifier: "%.2f")")
+                                .fontWeight(.medium)
+                                .font(.system(size: 12))
+                        }
+                    }
+                Capsule()
+                    .strokeBorder(Color("BorderColor"), lineWidth: 1)
+                    .frame(width: 35, height: 25)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .opacity(0.6)
+                    .overlay {
+                        Text("\(movieDetail.originalLanguage.uppercased())")
+                            .fontWeight(.medium)
+                            .font(.system(size: 12))
+                    }
+                Capsule()
+                    .strokeBorder(Color("BorderColor"), lineWidth: 1)
+                    .frame(width: 50, height: 25)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .opacity(0.6)
+                    .overlay {
+                        Text("\(movieDetail.runtime) M")
+                            .fontWeight(.medium)
+                            .font(.system(size: 12))
+                    }
+                Capsule()
+                    .strokeBorder(Color("BorderColor"), lineWidth: 1)
+                    .frame(width: 95, height: 25)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .opacity(0.6)
+                    .overlay {
+                        Text("\(movieDetail.releaseDate)")
+                            .fontWeight(.medium)
+                            .font(.system(size: 12))
+                    }
+            }
+        }.padding(.leading)
     }
 }
 
 #Preview {
-    MovieInfoView()
+    MovieInfoView(movieDetail: Mockdata.movieDetailsample)
 }
